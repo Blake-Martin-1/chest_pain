3d734cd920e18c20f51f1d3afc69d0a21aaf9945
Blake Martin <Blake.Martin@childrenscolorado.org>
2026-03-05 16:07:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# chest_pain
+Study evaluating ChatGPT to evaluate clinical notes of children with chest pain in cardiology clinic to determine indication for echocardiography
---
+++ cp_main.R
@@ -0,0 +1,4 @@
+### Code to load in chest pain clinical note files, create jsonl file, send to OpenAI for analysis via API, and store
+### the returned results
+
+df <- read_xlsx(path = "/phi/sbi/chest_pain/sample_cp.xlsx")
